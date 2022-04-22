8232640d6c6454a6dc47d1881f9ae341971b1242
Lindsay Ferreira <lindsayferreira@ext.cvccorp.com.br>
2022-04-22 18:16:38 -0300

    feat: update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import { SubscribeButton } from "./components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +9,21 @@ export default function Home() {
       <Head>
         <title>Fofonews | Início</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles["content-container"]}>
+        <section className={styles.hero}>
+          <span>👏 Hey, turu pom?</span>
+          <h1>
+            Apenas conteúdos <span> edificantes</span>
+          </h1>
+          <p>
+            Acesse todas as publicações <br />
+            por apenas <span>R$ 0.99</span> ao mês
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
